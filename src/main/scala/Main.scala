295e6d6f24e4933f524bd9cf8ed2a74ddca5cf1c
Vivian Berger <vivianberger028@gmail.com>
2022-10-07 09:06:11 +0200

    Added amount of jokers and points.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -63,34 +63,43 @@ def main(): Unit = {
     )
   )
 
+  var overallPoints = 0
+  var remainingJokers = 2
   var correctAnswerCount = 0
   for (question <- questions) {
-    val result = askQuestion(question)
+    val (result, jokerUsed, points) = askQuestion(question, remainingJokers)
     if (result == true) {
       correctAnswerCount = correctAnswerCount + 1
+      overallPoints += points
     }
+    if (jokerUsed == true) {
+      remainingJokers -= 1
+    }
+
   }
 
   println(s"Number of correctly answered questions: $correctAnswerCount!")
-
+  println(s"Points: $overallPoints")
 }
 
-def askQuestion(question: Question): Boolean = {
+def askQuestion(question: Question, remainingJokers: Int): (Boolean, Boolean, Int) = {
   println(question.text)
 
   for ((answer, text) <- question.answers) {
     println(text)
   }
 
-  println("J: Joker")
+  if (remainingJokers > 0) {
+    println(s"J: Joker (übrig: $remainingJokers)")
+  }
 
   val answer = Console.in.readLine().toLowerCase()
 
   if (question.correctAnswer.matches(answer)) {
     println("Correct!")
-    true
+    (true, false, 100)
   }
-  else if (answer == "j") {
+  else if (answer == "j" && remainingJokers > 0) {
     val correctAnswer = (question.correctAnswer, question.answers(question.correctAnswer))
     val wrongAnswer = Random.shuffle(question.answers
       .filter({ case (answer, text) => answer != question.correctAnswer})
@@ -105,16 +114,16 @@ def askQuestion(question: Question): Boolean = {
 
     if (question.correctAnswer.matches(answer)) {
       println("Correct!")
-      true
+      (true, true, 50)
     }
     else {
       println("Wrong...")
-      false
+      (false, true, 0)
     }
   }
   else {
     println("Wrong!")
-    false
+    (false, false, 0)
   }
 }
 
