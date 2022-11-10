a9c90b55aac70469e834df707f5b9e36baae9ef7
Vivian Berger <vivianberger028@gmail.com>
2022-11-10 17:28:13 +0100

    Configured gradle to run tests and added gradle wrapper to project and added workflow.

---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,11 @@
+import org.junit.runner.RunWith
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class MainSpec extends AnyFreeSpec {
+
+  "Should ask question" in {
+    
+  }
+}
